38b99b6fa62babf69758b82469befdf75e12b7a2
serafimmatheus <matheus18serafim@gmail.com>
2026-03-21 21:20:26 -0300

    feat: Integrate Fastify and dotenv for server setup in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello World");
+import "dotenv/config";
+
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
+  return reply.status(200).send({ message: "Hello World" });
+});
+
+try {
+  fastify.listen(
+    { port: Number(process.env.PORT) || 3000 },
+    (err: Error | null, address: string) => {
+      if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+      }
+      fastify.log.info(`Server is running on ${address}`);
+    },
+  );
+} catch (error) {
+  fastify.log.error(error);
+  process.exit(1);
+}
