642e40ad8e6ba252f9160cdfbb0fa5a972d6cd20
serafimmatheus <matheus18serafim@gmail.com>
2026-03-22 10:06:16 -0300

    feat: Integrate better-auth for authentication and enhance Prisma schema with new models and fields

---
+++ src/lib/auth.ts
@@ -0,0 +1,23 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { openAPI } from "better-auth/plugins";
+
+import { PrismaClient } from "../generated/prisma/client.js";
+
+const prisma = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+  adapter: new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+  }),
+});
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [openAPI()],
+});
